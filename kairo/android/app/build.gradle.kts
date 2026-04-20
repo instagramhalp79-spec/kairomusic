be30e60af67b444308947a68ec7ce7c860a401cc
home2652003 <home2652003@gmail.com>
2026-04-20 04:37:52 +0000

    Fix: Update dependencies to resolve build errors

--- kairo/android/app/build.gradle.kts
+++ kairo/android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.kairo"
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
